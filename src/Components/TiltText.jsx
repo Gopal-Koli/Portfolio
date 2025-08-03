bd9a4a0f62447016384eaa10a2463abb90754e53
Gopal-Koli <gopalkoli2715@gmail.com>
2025-08-03 23:22:11 +0530

    Footerpage added mobile responsive remaning3

--- src/Components/TiltText.jsx
+++ src/Components/TiltText.jsx
@@ -2,12 +2,17 @@ import React from "react";
 
 const Tilttext = (props) => {
   return (
-    <div className='tiltDiv cursor-grab lg:mt-40 ml-15 t' ref={props.abc}>
-      <h1 className='text-[4vw] uppercase font-[anzo3] leading-[4.2vw]'>
-        I am <span className='text-[black] shadow-white '>CLEAN CODE</span>™
+    <div
+      className="tiltDiv cursor-grab lg:mt-40 ml-15 t"
+      ref={props.abc}
+    >
+      <h1 className="text-[4vw] uppercase font-[anzo3] leading-[4.2vw]">
+        I am <span className="text-[black] shadow-white">CLEAN CODE</span>™
       </h1>
-      <h1 className='text-[6vw] font-[anzo3] leading-[7vw] '>DEVELOPER</h1>
-      <h1 className='text-[4vw] uppercase font-[anzo3] leading-[4vw]'>To hire</h1>
+      <h1 className="text-[6vw] font-[anzo3] leading-[7vw]">DEVELOPER</h1>
+      <h1 className="text-[4vw] uppercase font-[anzo3] leading-[4vw]">
+        To hire
+      </h1>
     </div>
   );
 };
--- src/pages/Page1.jsx
+++ src/pages/Page1.jsx
@@ -9,39 +9,55 @@ import { RiCodeSSlashLine } from "react-icons/ri";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
-
 const Page1 = () => {
+  const [xVal, setXVal] = useState(0);
+  const [yVal, setYVal] = useState(0);
+  const tiltRef = useRef(null);
 
-const [xVal,setXVal]=useState(0)
-const [yVal,setYVal]=useState(0)
-  const MouseMoving=(e)=>{
-    setXVal((e.clientX -tiltRef.current.getBoundingClientRect().x - tiltRef.current.getBoundingClientRect().width/2)/50)
-     setYVal(-(e.clientY -tiltRef.current.getBoundingClientRect().y - tiltRef.current.getBoundingClientRect().height/2)/10)
+  const MouseMoving = (e) => {
+    setXVal(
+      (e.clientX -
+        tiltRef.current.getBoundingClientRect().x -
+        tiltRef.current.getBoundingClientRect().width / 2) /
+        50
+    );
+    setYVal(
+      -(
+        e.clientY -
+        tiltRef.current.getBoundingClientRect().y -
+        tiltRef.current.getBoundingClientRect().height / 2
+      ) / 10
+    );
 
-    tiltRef.current.style.transform=`rotateX(${yVal}deg) rotateY(${xVal}deg)`
-  }
-  const tiltRef=useRef(null)
+    tiltRef.current.style.transform = `rotateX(${yVal}deg) rotateY(${xVal}deg)`;
+  };
 
-  
-  useGSAP(function(){
-    gsap.to(tiltRef.current,{
-      transform : `rotateX(${yVal}deg) rotateY(${xVal}deg)`,
-      duration:3,
-      ease:'power4.out'
+  useGSAP(
+    function () {
+      gsap.to(tiltRef.current, {
+        transform: `rotateX(${yVal}deg) rotateY(${xVal}deg)`,
+        duration: 3,
+        ease: "power4.out",
+      });
+    },
+    [xVal, yVal]
+  );
 
-    })
-  },[xVal,yVal])
-  return ( 
-    <div id="page1" onMouseMove={(e)=>{
-      MouseMoving(e)
-    }} className="lg:h-screen  relative px-8 py-8 bg-white ">
-               
-      <div id="page1-in" className="relative h-full sm:p-11 w-full rounded-4xl bg-gradient-to-r from-black to-white   shadow-2xl transition-transform duration-300 shadow-gray-500">
-
-        <RiCodeSSlashLine className="text-white text-[30px]"/>
-        <img src="" className="text-white text-[40px] ml "/>
-     <Tilttext  abc={tiltRef}/>
-      <Page1Bottom/>
+  return (
+    <div
+      id="page1"
+      onMouseMove={(e) => {
+        MouseMoving(e);
+      }}
+      className="lg:h-screen relative px-8 py-8 bg-white"
+    >
+      <div
+        id="page1-in"
+        className="relative h-full sm:p-11 w-full rounded-4xl bg-gradient-to-r from-black to-white shadow-2xl transition-transform duration-300 shadow-gray-500"
+      >
+        <RiCodeSSlashLine className="text-white text-[30px]" />
+        <Tilttext abc={tiltRef} />
+        <Page1Bottom />
       </div>
     </div>
   );
